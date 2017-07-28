6ac6e3559d8d5fcb5129ce89a8f4e425ace84d67
GOLF_PANACHAI_NIYOMKODCHAKORN <panachai.ny@gmail.com>
2017-07-28 23:51:37 +0700

    Added Innitial, Validate,

--- app/src/main/java/com/example/panachainy/firebaserealtimedatabase/Login_RegisterActivity.java
+++ app/src/main/java/com/example/panachainy/firebaserealtimedatabase/Login_RegisterActivity.java
@@ -1,13 +1,82 @@
 package com.example.panachainy.firebaserealtimedatabase;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login_RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private TextInputLayout layoutEmail, layoutPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__register);
+
+        Initial();
+
     }
+
+    void Initial() {
+        mAuth = FirebaseAuth.getInstance();
+//        (TextInputLayout) findViewById(R.id.layout_password);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+
+        layoutEmail = (TextInputLayout) findViewById(R.id.layoutEmail);
+        layoutPassword = (TextInputLayout) findViewById(R.id.layoutPassword);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //User is signed in
+                } else {
+                    //User is signed out
+                }
+
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private boolean validateForm() {
+        if (TextUtils.isEmpty(etEmail.getText().toString())) {
+            mLayoutEmail.setError("Required.");
+            return false;
+        } else if (TextUtils.isEmpty(etPassword.getText().toString())) {
+            mLayoutPassword.setError("Required.");
+            return false;
+        } else {
+            mLayoutEmail.setError(null);
+            mLayoutPassword.setError(null);
+            return true;
+        }
+    }
+
 }
